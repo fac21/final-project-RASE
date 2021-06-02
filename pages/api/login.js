82f52caed64137239d14d11f90560af507eff751
designaky <designaky@gmail.com>
2021-06-02 17:40:23 +0100

    Testing the cookie infons

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,25 +1,31 @@
-import { verifyUser, saveUserSession, COOKIE_OPTIONS } from "../../auth/auth";
+import {
+  verifyUser,
+  saveUserSession,
+  setCookie,
+  parseCookies,
+} from "../../auth/auth";
 
+export default async (req, res) => {
+  console.log(parseCookies(req)); // Checking the cookie
 
-export default async(req, res) => {
-    const method = req.method;
-    switch (method) {
-        case "GET":
-            {
-                res.status(500).json({ message: "Sorry error" });
-                break;
-            }
-        case "POST":
-            {
-                const { email, password } = req.body;
-                const user = await verifyUser(email, password);
-                const sid = await saveUserSession(user);
-                res.redirect("/");
-                break;
-            }
-        default:
-            {
-                res.status(500).json({ message: "Sorry error" });
-            }
+  const method = req.method;
+  switch (method) {
+    case "GET": {
+      res.status(500).json({ message: "Sorry error" });
+      break;
     }
+    case "POST": {
+      const { email, password } = req.body;
+      const user = await verifyUser(email, password);
+      const sid = await saveUserSession(user);
+
+      setCookie(res, "sid", sid);
+
+      res.redirect("/");
+      break;
+    }
+    default: {
+      res.status(500).json({ message: "Sorry error" });
+    }
+  }
 };
