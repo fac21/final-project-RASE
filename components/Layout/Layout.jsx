a11b2bc85738052da7cec4a1585d04e98ce57bd6
designaky <designaky@gmail.com>
2021-06-02 16:22:02 +0100

    testing

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -8,6 +8,17 @@ export default function Layout({ children, home }) {
   return (
     <div>
       <Head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossorigin=""
+        />
+        <script
+          src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+          integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+          crossorigin=""
+        ></script>
         <meta
           name="description"
           content="an app allowing users to search and contribute itineraries for UK-based trips"
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import db from "../database/connection";
 import Layout from "../components/Layout/Layout.jsx";
+import styles from "../styles/Home.module.css";
+import dynamic from "next/dynamic";
 
 export default function Home({ data }) {
+  const MapWithNoSSR = dynamic(() => import("../components/Map/Map"), {
+    ssr: false,
+  });
   return (
     <Layout home>
       <h1>travel app</h1>
+      <div id="mapid">
+        <MapWithNoSSR />
+      </div>
     </Layout>
   );
 }
