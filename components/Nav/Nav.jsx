6eb1d126a7ec0b18652e72cac944304eac7a805f
sakireid <59439482+haginom@users.noreply.github.com>
2021-06-03 17:30:38 +0100

    changing styled componenets

--- components/Nav/Nav.jsx
+++ components/Nav/Nav.jsx
@@ -1,17 +1,35 @@
 import Link from "next/link";
 import Burger from "../Burger/burger";
 import Menu from "../Menu/menu.jsx";
+import styled from "styled-components";
+
+const StyledMenu = styled.div`
+  position: relative;
+  background: hotpink;
+  height: fit-content;
+`;
+const StyledLink = styled.h1`
+  max-width: 40rem;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  text-decoration: underline;
+  padding: 1rem;
+`;
 
 export default function Nav({ open, setOpen }) {
   return (
-    <>
+    <StyledMenu>
       <div>
         <Burger open={open} setOpen={setOpen} />
         <Menu open={open} setOpen={setOpen} />
       </div>
-      <Link href="/">
-        <a>UK TRAVEL GUIDE</a>
-      </Link>
-    </>
+      <StyledLink>
+        <Link href="/">
+          <a className="navTitle">UK TRAVEL GUIDE</a>
+        </Link>
+      </StyledLink>
+      <div className="underline"></div>
+    </StyledMenu>
   );
 }
