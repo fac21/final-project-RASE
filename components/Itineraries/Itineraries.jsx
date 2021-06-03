1badddc0e4c7287e142abfb23d99f405554bbae5
sakireid <59439482+haginom@users.noreply.github.com>
2021-06-03 16:45:38 +0100

    loading itinearries
    Co-authored-by: Evgeny Pletnev <e.a.pletnev@gmail.com>

---
+++ components/Itineraries/Itineraries.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Itinerary from "../../pages/itineraries";
+
+export default function Itineraries({ data }) {
+  console.log("test", data);
+  // return <h1>Hello</h1>;
+
+  const itineraries = data.map((itinerary) => {
+    console.log(itinerary);
+    return (
+      <section>
+        <h1>
+          <a href="/">{itinerary.name}</a>
+        </h1>
+        <p>{itinerary.duration} days</p>
+        <p>{itinerary.need_car ? "need car" : "don't need car"}</p>
+        <p>£{itinerary.budget}</p>
+      </section>
+    );
+  });
+
+  return <>{itineraries}</>;
+
+  // const donutArray = data.map((itinerary) => {
+  //   return (
+  //     <h1>Test</h1>
+  //     //   <section className="eachDonut" key={donut.id}>
+  //     //     <h1 className="donut__name">{donut.name}</h1>
+  //     //     <img
+  //     //       className="donut__image"
+  //     //       src={donut.image}
+  //     //       alt={"picture of " + donut.name + " donut"}
+  //     //     />
+  //     //     <p className="donut__description">{donut.description}</p>
+  //     //     <p className="donut__price">£{donut.price.toFixed(2)}</p>
+  //     //     <button className={donut.name} onClick={addDonut}>
+  //     //       Gimme a Donut
+  //     //     </button>
+  //     // </section>
+  //   );
+  // }
+}
